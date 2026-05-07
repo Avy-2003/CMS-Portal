7355519b067f2b856b013601c1b77b5737335bf3
Abhishekgowda V Y <abhishekgowda.vy@incture.com>
2026-05-07 22:04:12 +0530

    replaced local url

--- cms_frontend/cms-front/src/pages/AssignOfficerPage.jsx
+++ cms_frontend/cms-front/src/pages/AssignOfficerPage.jsx
@@ -13,10 +13,10 @@ export default function OfficersListPage() {
 
 
 
-  // fetch officers
+  // fetch officershttps://diplomatic-upliftment-production.up.railway.app
   useEffect(() => {
     axios
-      .get("http://localhost:8080/users/officers/stats")
+      .get("https://diplomatic-upliftment-production.up.railway.app/users/officers/stats")
       .then((res) => {
         console.log("OFFICERS:", res.data);
         setOfficers(res.data);
@@ -31,7 +31,7 @@ export default function OfficersListPage() {
 
     axios
       .put(
-        `http://localhost:8080/complaints/${complaintIds[officerId]}/assign?officerId=${officerId}`
+        `https://diplomatic-upliftment-production.up.railway.app/complaints/${complaintIds[officerId]}/assign?officerId=${officerId}`
       )
       .then(() => {
         alert("Complaint Assigned Successfully");
